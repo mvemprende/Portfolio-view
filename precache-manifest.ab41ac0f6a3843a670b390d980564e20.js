fa2362cc414ac1ab4dc1b4a1d7fd25a3c5bedbd6
John Vincent <johnvuc@hotmail.com>
2020-07-08 16:27:57 +0200

    initilize

---
+++ precache-manifest.ab41ac0f6a3843a670b390d980564e20.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5b1e70fd80eec95ebd9798fa1eb851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4adc4460f4f293143c4",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0f02916aba411e569146",
+    "url": "/static/css/main.218b6d43.chunk.css"
+  },
+  {
+    "revision": "f4adc4460f4f293143c4",
+    "url": "/static/js/2.9ea58179.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.9ea58179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f02916aba411e569146",
+    "url": "/static/js/main.548f1d33.chunk.js"
+  },
+  {
+    "revision": "78753c517123361ae7ca",
+    "url": "/static/js/runtime-main.c1d8045f.js"
+  },
+  {
+    "revision": "336affd846eedde6ec6e83f9416746fa",
+    "url": "/static/media/AMBIDESTRO-01.336affd8.svg"
+  },
+  {
+    "revision": "ab65ded313fe53c8194ac3b0446d9d92",
+    "url": "/static/media/Bitly.ab65ded3.png"
+  },
+  {
+    "revision": "153381d7ae49e6f71f23d7a6940d4c5a",
+    "url": "/static/media/GRUPOAMANECER-02.153381d7.svg"
+  },
+  {
+    "revision": "f4cd399ffe519855db2d305fbc4e178f",
+    "url": "/static/media/Trello.f4cd399f.png"
+  },
+  {
+    "revision": "dc2ad1495d028924ebaa8daf8d5ad79f",
+    "url": "/static/media/armando-01.dc2ad149.png"
+  },
+  {
+    "revision": "a0989ee95d59c049725c7fe1933cb1b8",
+    "url": "/static/media/canva-modelo-mv.a0989ee9.png"
+  },
+  {
+    "revision": "80e85990ea663d773a3f14734f22bc9e",
+    "url": "/static/media/canva.80e85990.png"
+  },
+  {
+    "revision": "d5ed23249db7107935a80aebef45dfbc",
+    "url": "/static/media/caryarit-01.d5ed2324.png"
+  },
+  {
+    "revision": "a3c90d71703a8f395beee816dfe90607",
+    "url": "/static/media/cohete2-01.a3c90d71.png"
+  },
+  {
+    "revision": "735dcd511d1aece727b30e1f3b1c10a6",
+    "url": "/static/media/cuadro-recursos-3-01.735dcd51.png"
+  },
+  {
+    "revision": "0b34591e6540e5aa96bfa6d16cfad88f",
+    "url": "/static/media/diana-01.0b34591e.png"
+  },
+  {
+    "revision": "30492913dc83c6d398a7e433c32cbecd",
+    "url": "/static/media/feedly-logo.30492913.png"
+  },
+  {
+    "revision": "0846e93cf69ba342edbd8d9b2749034d",
+    "url": "/static/media/grupomiranda-01.0846e93c.png"
+  },
+  {
+    "revision": "5725acf7b89f8a6cd4431152ba9b7f91",
+    "url": "/static/media/grupomiranda-03.5725acf7.svg"
+  },
+  {
+    "revision": "b960013d219297b4d5f3659ca25f60b9",
+    "url": "/static/media/icuida-logo.b960013d.png"
+  },
+  {
+    "revision": "7b7d4a6130fbafa71f4210f53d2a0005",
+    "url": "/static/media/logo-mv.7b7d4a61.png"
+  },
+  {
+    "revision": "8c58b1feb94b2b9534b6e20a142d5df2",
+    "url": "/static/media/logo-mv.8c58b1fe.jpg"
+  },
+  {
+    "revision": "1175e582e137ebef5757b4ffb0d0d16f",
+    "url": "/static/media/luis-01.1175e582.png"
+  },
+  {
+    "revision": "3beefcbcbd12e5e4e7e7dc926eb7e4c5",
+    "url": "/static/media/maka-01.3beefcbc.png"
+  },
+  {
+    "revision": "1e960912538c6cd2ce74f71c54424428",
+    "url": "/static/media/mv-consultor.1e960912.jpg"
+  },
+  {
+    "revision": "6185c0e8624daefdffba6e784ecc7eaf",
+    "url": "/static/media/mv-consultora-de-negocio.6185c0e8.png"
+  },
+  {
+    "revision": "73fe4bd139b9665e4b0397bcba756c76",
+    "url": "/static/media/mv-consultora-de-negocio.73fe4bd1.jpg"
+  },
+  {
+    "revision": "0b4e01aa6146030358b9904e82833438",
+    "url": "/static/media/mv-consultora-startup.0b4e01aa.jpg"
+  },
+  {
+    "revision": "338ffcecea55c15d60ae5e90d4a6a068",
+    "url": "/static/media/mv-consultora.338ffcec.jpg"
+  },
+  {
+    "revision": "07cec45d3e9ab8c078e9303a17058e3d",
+    "url": "/static/media/mv-emprendimiento.07cec45d.JPG"
+  },
+  {
+    "revision": "9800199a341282d31239f281e1f8f481",
+    "url": "/static/media/mv-emprendimiento.9800199a.jpg"
+  },
+  {
+    "revision": "3d8a5cfc66cfb5cf746e50f991c66464",
+    "url": "/static/media/mv-financiacion-startup.3d8a5cfc.jpg"
+  },
+  {
+    "revision": "28891caf8b667ddf92f39826dead8d51",
+    "url": "/static/media/mv-mapa.28891caf.png"
+  },
+  {
+    "revision": "6b48a7ba558b62a7da521beb18e690e0",
+    "url": "/static/media/mv-project-manager.6b48a7ba.jpg"
+  },
+  {
+    "revision": "ae1f6d818f4fc2d621ded4a655338ff6",
+    "url": "/static/media/mv-project-manager.ae1f6d81.jpg"
+  },
+  {
+    "revision": "1a652d88345dedbb9103952c094af47d",
+    "url": "/static/media/mv-sobre-mi.1a652d88.png"
+  },
+  {
+    "revision": "5dfa83faaf456e8d07873db047aa6e6e",
+    "url": "/static/media/nowhome-06.5dfa83fa.svg"
+  },
+  {
+    "revision": "a3e9d7ffcb26de1e20ab29ae9a30b50d",
+    "url": "/static/media/pablo-01.a3e9d7ff.png"
+  },
+  {
+    "revision": "70b8a18bff5e2901f6d6745e6285dba1",
+    "url": "/static/media/recursos1-01.70b8a18b.png"
+  },
+  {
+    "revision": "d5658517b2055a030d87157e05293b85",
+    "url": "/static/media/rene-01-01.d5658517.png"
+  },
+  {
+    "revision": "d1a066b3315bde32e342c1e292586834",
+    "url": "/static/media/slack.d1a066b3.png"
+  },
+  {
+    "revision": "1478ff9ed47cf9fdadb97d742a70cf44",
+    "url": "/static/media/spark-post.1478ff9e.jpg"
+  },
+  {
+    "revision": "64c94df1c94256af57a1683f1b7a952a",
+    "url": "/static/media/sweetstories-01.64c94df1.png"
+  },
+  {
+    "revision": "7876841cb2d5f9a75856b6b30aafecb6",
+    "url": "/static/media/sweetstories-04.7876841c.svg"
+  },
+  {
+    "revision": "eeb43fd81d6d414831ccc0c67ef8c824",
+    "url": "/static/media/xmind.eeb43fd8.png"
+  }
+]);
